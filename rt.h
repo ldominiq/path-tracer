953d02cc864e283967948e007ec34ece0b89ccec
Lucas <lucasdominique99@gmail.com>
2026-08-12 15:00:40 +0200

    random number utilities

--- rt.h
+++ rt.h
@@ -2,9 +2,11 @@
 #define RT_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
+#include <random>
 
 using std::make_shared;
 using std::shared_ptr;
@@ -18,6 +20,17 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+	// returns a random real in [min, max)
+	return min + (max - min) * random_double();
 }
 
 // common headers
