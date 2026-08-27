5794eaeeee84ad6bb0d5aa4c7952f67d7d18949b
Lucas <lucasdominique99@gmail.com>
2026-08-27 19:59:15 +0200

    Bounding Volume Hierarchies to make render faster but i'm a bit confused...

--- rt.h
+++ rt.h
@@ -33,6 +33,11 @@ inline double random_double(double min, double max) {
 	return min + (max - min) * random_double();
 }
 
+inline int random_int(int min, int max) {
+	// returns a random integer in [min,max]
+	return static_cast<int>(random_double(min, max + 1));
+}
+
 // common headers
 
 #include "color.h"
