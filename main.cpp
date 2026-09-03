bd4bb33e6650faad7b8a31b30713a2832a479ea6
Lucas <lucasdominique99@gmail.com>
2026-09-03 13:57:24 +0200

    perlin noise

--- main.cpp
+++ main.cpp
@@ -159,8 +159,33 @@ void moon_and_earth() {
 	cam.render(world);
 }
 
+void perlin_spheres() {
+	hittable_list world;
+
+	auto pertext = make_shared<noise_texture>();
+
+	world.add(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+	world.add(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
+
+	camera cam;
+
+	cam.aspect_ratio = 16.0 / 9.0;
+	cam.image_width = 400;
+	cam.samples_per_pixel = 100;
+	cam.max_depth = 50;
+
+	cam.vfov = 20;
+	cam.lookfrom = point3(13, 2, 3);
+	cam.lookat = point3(0, 0, 0);
+	cam.vup = vec3(0, 1, 0);
+
+	cam.defocus_angle = 0;
+
+	cam.render(world);
+}
+
 int main() {
-	switch (4) {
+	switch (5) {
 		case 1:
 			bouncing_spheres();
 			break;
@@ -172,6 +197,9 @@ int main() {
 			break;
 		case 4:
 			moon_and_earth();
+			break;
+		case 5:
+			perlin_spheres();
 			break;
 	}
 }
