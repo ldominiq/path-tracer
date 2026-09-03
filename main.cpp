f002fdae4d271c3cb3a94212a94728a30bb5da29
Lucas <lucasdominique99@gmail.com>
2026-09-03 15:08:27 +0200

    generated marble texture

--- main.cpp
+++ main.cpp
@@ -170,8 +170,8 @@ void perlin_spheres() {
 	camera cam;
 
 	cam.aspect_ratio = 16.0 / 9.0;
-	cam.image_width = 400;
-	cam.samples_per_pixel = 100;
+	cam.image_width = 1200;
+	cam.samples_per_pixel = 400;
 	cam.max_depth = 50;
 
 	cam.vfov = 20;
